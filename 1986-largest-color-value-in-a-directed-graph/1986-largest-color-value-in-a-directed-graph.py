7bebd9c1414a59d9892ccf681b4b0a81becc5e78
rishitha-2406 <shettyrishi697@gmail.com>
2025-05-26 21:04:00 +0530

    Time: 1885 ms (64.27%), Space: 79.2 MB (92.81%) - LeetHub

---
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.py
@@ -0,0 +1,32 @@
+class Solution:
+  def largestPathValue(self, colors: str, edges: list[list[int]]) -> int:
+    n = len(colors)
+    ans = 0
+    processed = 0
+    graph = [[] for _ in range(n)]
+    inDegrees = [0] * n
+    q = collections.deque()
+    count = [[0] * 26 for _ in range(n)]
+
+    
+    for u, v in edges:
+      graph[u].append(v)
+      inDegrees[v] += 1
+
+    for i, degree in enumerate(inDegrees):
+      if degree == 0:
+        q.append(i)
+
+    while q:
+      u = q.popleft()
+      processed += 1
+      count[u][ord(colors[u]) - ord('a')] += 1
+      ans = max(ans, count[u][ord(colors[u]) - ord('a')])
+      for v in graph[u]:
+        for i in range(26):
+          count[v][i] = max(count[v][i], count[u][i])
+        inDegrees[v] -= 1
+        if inDegrees[v] == 0:
+          q.append(v)
+
+    return ans if processed == n else -1
