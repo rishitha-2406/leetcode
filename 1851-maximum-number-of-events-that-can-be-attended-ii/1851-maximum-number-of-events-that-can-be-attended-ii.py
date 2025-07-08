d0d87a8c037ba21d5e508573258042d542f76790
rishitha-2406 <shettyrishi697@gmail.com>
2025-07-08 22:29:39 +0530

    Time: 2372 ms (9.33%), Space: 285.8 MB (8.79%) - LeetHub

---
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+  def maxValue(self, events: list[list[int]], k: int) -> int:
+    events.sort()
+
+    @functools.lru_cache(None)
+    def dp(i: int, k: int) -> int:
+      
+      if k == 0 or i == len(events):
+        return 0
+
+      j = bisect.bisect(events, [events[i][1], math.inf, math.inf], i + 1)
+      return max(events[i][2] + dp(j, k - 1), dp(i + 1, k))
+
+    return dp(0, k)
